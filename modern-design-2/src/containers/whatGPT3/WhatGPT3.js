402cba13295515c08ac47c7bf6dd4115ecea3d0f
Nhoj <nhojplummer@yahoo.com>
2023-04-05 12:07:13 -0400

    Setting up the next section for whatgpt3

--- modern-design-2/src/containers/whatGPT3/WhatGPT3.js
+++ modern-design-2/src/containers/whatGPT3/WhatGPT3.js
@@ -1,8 +1,15 @@
 import React from "react"
 import "./whatGPT3.css"
+import { Feature } from "../../components"
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+    </div>
+  )
 }
 
 export default WhatGPT3
