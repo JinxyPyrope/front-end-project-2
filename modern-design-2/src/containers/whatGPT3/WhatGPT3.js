c4d8865695dcbcfe0c93a36c7e56f264b6d292b1
Nhoj <nhojplummer@yahoo.com>
2023-04-05 12:29:25 -0400

    Gradient styling added to h1 text in component

--- modern-design-2/src/containers/whatGPT3/WhatGPT3.js
+++ modern-design-2/src/containers/whatGPT3/WhatGPT3.js
@@ -8,6 +8,15 @@ const WhatGPT3 = () => {
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
