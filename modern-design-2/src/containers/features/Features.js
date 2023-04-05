6464b702bf762eed29383d8467e2fe9d3072d2dd
Nhoj <nhojplummer@yahoo.com>
2023-04-05 13:44:01 -0400

    Had to fix locataion of code to different feature. All files fixed now

--- modern-design-2/src/containers/features/Features.js
+++ modern-design-2/src/containers/features/Features.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react"
 
-const Features = () => {
-  return (
-    <div>Features</div>
-  )
+function Features() {
+  return <div>Features</div>
 }
 
 export default Features
