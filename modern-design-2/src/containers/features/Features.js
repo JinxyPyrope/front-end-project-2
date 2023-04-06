6270cc7ee5d5c8da9d05aaecc3ae045bfb16aabc
Nhoj <nhojplummer@yahoo.com>
2023-04-06 13:17:02 -0400

    Started on the Features component for the next section

--- modern-design-2/src/containers/features/Features.js
+++ modern-design-2/src/containers/features/Features.js
@@ -1,7 +1,17 @@
 import React from "react"
 
 function Features() {
-  return <div>Features</div>
+  return (
+    <div className="gpt3__features section__padding">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        
+      </div>
+    </div>
+  )
 }
 
 export default Features
