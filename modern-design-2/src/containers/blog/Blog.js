6533e9dd98dae9388ff3b16e854850a368536506
Nhoj <nhojplummer@yahoo.com>
2023-04-09 13:36:23 -0400

    Article reusable compoenent added but needed a bit more styling

--- modern-design-2/src/containers/blog/Blog.js
+++ modern-design-2/src/containers/blog/Blog.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Article } from "../../components"
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports"
 import "./blog.css"
 
 const Blog = () => {
@@ -9,8 +10,12 @@ const Blog = () => {
         <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
       </div>
       <div className="gpt3__blog-container">
-        <div className="gpt3__blog-container_groupA">Article</div>
-        <div className="gpt3__blog-container_groupB">Article Article Article Article</div>
+        <div className="gpt3__blog-container_groupA">
+          <Article imgUrl={blog01} date="Sept 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article imgUrl={blog02} date="Sept 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" /> <Article imgUrl={blog03} date="Sept 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" /> <Article imgUrl={blog04} date="Sept 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" /> <Article imgUrl={blog05} date="Sept 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
       </div>
     </div>
   )
